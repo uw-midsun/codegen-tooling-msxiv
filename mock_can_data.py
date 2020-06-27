6b42ba91b540afe6c32a82f4821f27a58f318261
Avery Chiu <a27chiu@uwaterloo.ca>
2020-06-27 18:05:37 +0000

    Randomized message sending and resolved changes

--- mock_can_data.py
+++ mock_can_data.py
@@ -16,29 +16,45 @@ import time
 import random
 
 sleep_time_s = 1
-db = cantools.database.load_file('system_can.dbc')
+can_messages = []
+
+try:
+    db = cantools.database.load_file('system_can.dbc')
+except:
+    print("Must generate DBC file first")
+    print("Run make gen && make gen-dbc")
 
 # This can be edited depending on the CAN interface
 can_bus = can.interface.Bus('vcan0', bustype='socketcan')
 
-def send_message(msg,msg_data):
+def main():
+    num_messages_to_send = int(input("Please enter the number of random CAN messages you would like to send: "))
+    get_messages()
+    iterate_message_and_signal(num_messages_to_send)
+
+def get_messages():
+    for msg in db.messages:
+        can_messages.append(msg)
+
+def iterate_message_and_signal(num_messages):
+    num_messages_sent = 0
+    while num_messages_sent < num_messages:
+        msg = random.choice(can_messages)
+        num_messages_sent +=1
+        data = {}
+        for signal in msg.signals:
+            if signal.is_multiplexer:
+                data[signal.name]=random.randint(0,5)
+            else:
+                data[signal.name]=random.randint(0, pow(2,signal.length)-1)
+        send_message(msg,data)
+        time.sleep(sleep_time_s)
+    print(str(num_messages_sent) + " CAN messages have been sent")
+
+def send_message(msg, msg_data):
     new_data = msg.encode(msg_data)
     message = can.Message(arbitration_id=msg.frame_id, data=new_data)
     can_bus.send(message)
-
-def iterate_message_and_signal():
-    for msg in db.messages:
-        data = {}
-        print(msg.name)
-        for signal in msg.signals:
-            # Send random data for each signal
-            data[str(signal.name)]=random.randint(0, 5)
-        print(data)
-        send_message(msg,data)
-        time.sleep(sleep_time_s)
-
-def main():
-    iterate_message_and_signal()
 
 if __name__ == "__main__":
     main()
