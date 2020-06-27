ec16c8149cf8e23b68ab286ff0d13c33d3479c25
Avery Chiu <a27chiu@uwaterloo.ca>
2020-06-27 15:46:13 +0000

    Added instructions on how to use script

--- mock_can_data.py
+++ mock_can_data.py
@@ -1,3 +1,15 @@
+# This script automatically generates and sends CAN messages based off of the DBC file
+# Ensure that you have can and cantools installed, which can be accomplished by running
+# pip install -r requirements.txt
+# Alternatively you could also just run
+# pip install can && pip install cantools
+
+# If you are running this script with virtual CAN ensure that it is set up first
+# You can run the below commands
+# sudo modprobe vcan
+# sudo ip link add dev vcan0 type vcan
+# sudo ip link set up vcan0
+
 import cantools
 import can
 import time
@@ -5,6 +17,8 @@ import random
 
 sleep_time_s = 1
 db = cantools.database.load_file('system_can.dbc')
+
+# This can be edited depending on the CAN interface
 can_bus = can.interface.Bus('vcan0', bustype='socketcan')
 
 def send_message(msg,msg_data):
@@ -17,7 +31,8 @@ def iterate_message_and_signal():
         data = {}
         print(msg.name)
         for signal in msg.signals:
-            data[str(signal.name)]=random.randint(0, 64)
+            # Send random data for each signal
+            data[str(signal.name)]=random.randint(0, 5)
         print(data)
         send_message(msg,data)
         time.sleep(sleep_time_s)
