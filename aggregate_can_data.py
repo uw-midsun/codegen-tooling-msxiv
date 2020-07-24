a3ce2cc4f7e030df646c5667c0dfbe6dd0d8df2b
AveryChiu64 <a27chiu@uwaterloo.ca>
2020-07-24 00:38:30 +0000

    Added section to write to csv

--- aggregate_can_data.py
+++ aggregate_can_data.py
@@ -1,4 +1,5 @@
-# This script recieves CAN data and sends it to FRED with MQTT
+# This script recieves CAN data,sends it to FRED with MQTT, and stores the data locally
+# This should be run by the RPI for the telemetry system
 
 # If you are running this script with virtual CAN ensure that it is set up first
 # You can run the below commands
@@ -8,15 +9,24 @@
 
 import cantools
 import can
+import csv
+from datetime import datetime
+import json
 import os
 import paho.mqtt.client as mqtt
-import json
-from datetime import datetime
 
 can_bus = can.interface.Bus('vcan0', bustype='socketcan')
 db = cantools.database.load_file('system_can.dbc')
 broker = "broker.hivemq.com"
+port = 1883
 client = mqtt.Client()
+
+#Write new line and header
+with open('can_messages.csv', 'a', newline='') as csvfile:
+    fieldnames = ['datetime','name','sender','data']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writerow({'datetime':'','name':'','sender':'','data':''})
+    writer.writeheader()
 
 def on_connect(client,userdata,flags,rc):
     if rc==0:
@@ -26,22 +36,35 @@ def on_connect(client,userdata,flags,rc):
 
 def connect():
     client.on_connect=on_connect
-    client.connect(broker,1883,60)
+    client.connect(broker,port,60)
     client.loop_start()
 
 def decode_can_message():
     message = can_bus.recv()
     decoded = db.decode_message(message.arbitration_id, message.data)
-    decoded['datetime'] = str(datetime.fromtimestamp(message.timestamp))
-    decoded['name'] = db.get_message_by_frame_id(message.arbitration_id).name
-    decoded['sender'] = db.get_message_by_frame_id(message.arbitration_id).senders[0]
-    #print(decoded)
-    client.publish("uwmidsun/can/test", payload=json.dumps(decoded))
+
+    time = str(datetime.fromtimestamp(message.timestamp))
+    name = db.get_message_by_frame_id(message.arbitration_id).name
+    sender = db.get_message_by_frame_id(message.arbitration_id).senders[0]
+
+    write_to_csv(time,name,sender,decoded)
+
+    decoded['datetime'] = time
+    decoded['name'] = name
+    decoded['sender'] = sender
+    return decoded
+
+def write_to_csv(time,name,sender,data):
+    with open('can_messages.csv', 'a', newline='') as csvfile:
+        fieldnames = ['datetime','name','sender','data']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow({'datetime':time,'name':name,'sender':sender,'data':data})
 
 def main():
     connect()
     while(True):
-        decode_can_message()
+        decoded = decode_can_message()
+        client.publish("uwmidsun/can/test", payload=json.dumps(decoded))
 
 if __name__ == "__main__":
     main()
