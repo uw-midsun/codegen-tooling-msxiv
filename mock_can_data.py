429dfcb54ee989face809ba2da94643d860457e3
Hewitt McGaughey <hewittmcgaughey@gmail.com>
2020-08-24 00:44:23 +0000

    quick edit to see if Travis works with this config

--- mock_can_data.py
+++ mock_can_data.py
@@ -1,7 +1,7 @@
 # This script automatically generates and sends CAN messages based off of the DBC file
 # Ensure that you have can and cantools installed, which can be accomplished by running
 # pip install -r requirements.txt
-# Alternatively you could also just run
+# Alternatively, you could also just run
 # pip install can && pip install cantools
 
 # If you are running this script with virtual CAN ensure that it is set up first
